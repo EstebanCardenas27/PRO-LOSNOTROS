f2bb1a7b4eb530b47b81685a2d87a02ea27971b6
Estebancardenas27 <estebancardenas2796@gmail.com>
2026-07-22 13:08:13 -0400

    feat ux-improvements V0.6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
 
   icons: {
     icon: [
-      { url: "/ico/favicon.ico" },
+      { url: "/ico/favicon.png" },
       { url: "/ico/favicon.svg", type: "image/svg+xml" },
       { url: "/ico/favicon-96x96.png", sizes: "96x96", type: "image/png" },
     ],
